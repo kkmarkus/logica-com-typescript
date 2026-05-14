9599503953fb3775fdd0c11dc3d4722c245dffe1
Markus <markusaurelius04@gmail.com>
2026-05-14 11:16:24 -0300

    update no calculo de imc

--- calculo-imc.ts
+++ calculo-imc.ts
@@ -1,30 +1,40 @@
 import readline = require("readline-sync")
 console.clear()
 
-// Coletando informacoes
-const nome:string = readline.question("Informe seu nome: ")
-const peso:number = Number(readline.question("Informe seu peso(kg): "))
-const altura:number = Number(readline.question("Informe sua altura(m): "))
+let loop:boolean = true
 
-// Calculando IMC
-const imc:number = peso / altura**2
+while(loop){
 
-// Classificacao
-let classificacao:string = ""
+	// Coletando informacoes
+	const nome:string = readline.question("Informe seu nome: ")
+	const peso:number = Number(readline.question("Informe seu peso(kg): "))
+	const altura:number = Number(readline.question("Informe sua altura(m): "))
 
-if (imc < 18.5){
-	classificacao = "Baixo peso"
-} else if (imc >= 18.5 && imc < 25){
-	classificacao = "Peso normal"
-} else if (imc >= 25 && imc < 30){
-	classificacao = "Excesso de peso"
-} else if (imc >= 30 && imc < 35){
-	classificacao = "Obesidade de classe 1"
-} else if (imc >= 35 && imc < 40){
-	classificacao = "Obesidade de classe 2"
-} else {
-	classificacao = "Obesidade de classe 3"
+	// Calculando IMC
+	const imc:number = peso / altura**2
+
+	// Classificacao
+	let classificacao:string = ""
+
+	if (imc < 18.5){
+		classificacao = "Baixo peso"
+	} else if (imc >= 18.5 && imc < 25){
+		classificacao = "Peso normal"
+	} else if (imc >= 25 && imc < 30){
+		classificacao = "Excesso de peso"
+	} else if (imc >= 30 && imc < 35){
+		classificacao = "Obesidade de classe 1"
+	} else if (imc >= 35 && imc < 40){
+		classificacao = "Obesidade de classe 2"
+	} else {
+		classificacao = "Obesidade de classe 3"
+	}
+
+	console.log(`\nOlá ${nome} \nPeso: ${peso}kg \nAltura: ${altura}m \nIMC(${imc}) classificado em: ${classificacao}`) 
+	
+	let continuar:string = readline.question("Deseja continua? S - Sim | N - Não\n")
+	if (continuar.toUpperCase() === "N"){
+		loop = false
+	}
 }
-
-console.log(`\nOlá ${nome} \nPeso: ${peso}kg \nAltura: ${altura}m \nIMC(${imc.toFixed(2)}) classificado em: ${classificacao}`) 
 
