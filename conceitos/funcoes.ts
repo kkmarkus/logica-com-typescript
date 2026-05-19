cfbfb12a1b6fdbc375221c815802ad92978bc8b4
Markus <markusaurelius04@gmail.com>
2026-05-19 10:51:12 -0300

    função

---
+++ conceitos/funcoes.ts
@@ -0,0 +1,5 @@
+function digaOla(nome:string = "Estranho"):string {
+  return "Olá, " + nome
+}
+
+console.log(digaOla("Markus"))
---
+++ exercicios/calculo.ts
@@ -0,0 +1,7 @@
+const {somar,subtrair} = require('./calculadora.ts')
+
+const a = 10
+const b = 5
+
+console.log("Soma:",somar(a, b))
+console.log("Subtração:", subtrair(a, b))
